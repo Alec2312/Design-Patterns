f10c1d3845fecd37d9a2b329f1308ca93a26bbe3
A <1207074@student.roc-nijmegen.nl>
2025-09-15 13:24:26 +0200

    Added unsubscribed observer

--- ObserverPattern/Program.cs
+++ ObserverPattern/Program.cs
@@ -7,14 +7,15 @@ namespace ObserverPattern
         static void Main(string[] args)
         {
             WeatherData weatherData = new WeatherData();
-            new CurrentConditionDisplay(weatherData);
-            new ForecastDisplay(weatherData);
-            new StatisticsDisplay(weatherData);
+            CurrentConditionDisplay currentCondition = new CurrentConditionDisplay(weatherData);
+            ForecastDisplay forecast = new ForecastDisplay(weatherData);
+            StatisticsDisplay statistics = new StatisticsDisplay(weatherData);
 
             // Create instances of displays 
+            weatherData.RemoveObserver(statistics);
             weatherData.SetMeasurements(28, 65, 30.4f);
             weatherData.SetMeasurements(29, 70, 29.2f);
-            weatherData.SetMeasurements(30, 90, 29.2f); 
+            weatherData.SetMeasurements(30, 90, 29.2f);
             Console.ReadLine();
         }
     }

66cd2e8dca5d24f2530b340400254daf12671802
A <1207074@student.roc-nijmegen.nl>
2025-09-22 14:39:43 +0200

    Added DecoratorPattern for beverages with condiments

---
+++ DecoratorPattern/Beverages/Chocolate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern.Beverages
+{
+    internal class Chocolate : Beverage
+    {
+        public Chocolate(Beverage beverage = null)
+        {
+            description = "Chocolate";
+            this.baseBeverage = beverage;
+
+        }
+        public override string GetDescription()
+        {
+            if (baseBeverage != null)
+            {
+                return baseBeverage.GetDescription() + ", " + description;
+            }
+            return description;
+        }
+        public override double cost()
+        {
+            double extra = 0.0;
+            switch (Size)
+            {
+                case Size.TALL: extra = 0.50; break;
+                case Size.GRANDE: extra = 0.70; break;
+                case Size.VENDI: extra = 0.90; break;
+            }
+
+            if (baseBeverage != null)
+            {
+                return 1.00 + extra + baseBeverage.cost();
+            }
+
+            return 1.00 + extra;
+        }
+    }
+}
---
+++ DecoratorPattern/Condiments/Ice.cs
@@ -0,0 +1,36 @@
+﻿using DecoratorPattern.Beverages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern.Condiments
+{
+    internal class Ice : CondimentDecorator
+    {
+        public Ice(Beverage beverage)
+        {
+            this.baseBeverage = beverage;
+        }
+
+        public override double cost()
+        {
+            double extra = 0;
+
+            switch (baseBeverage.Size)
+            {
+                case Size.TALL: extra = 0.10; break;
+                case Size.GRANDE: extra = 0.15; break;
+                case Size.VENDI: extra = 0.20; break;
+            }
+
+            return 1.00 + extra + baseBeverage.cost();
+        }
+
+        public override string GetDescription()
+        {
+            return baseBeverage.GetDescription() + ", Liqour";
+        }
+    }
+}

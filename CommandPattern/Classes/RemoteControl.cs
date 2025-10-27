533544c3432d07a9cdac71d3b097501716050585
A <1207074@student.roc-nijmegen.nl>
2025-10-27 13:44:53 +0100

    Added Command Pattern

---
+++ CommandPattern/Classes/RemoteControl.cs
@@ -0,0 +1,61 @@
+﻿using CommandPattern.Classes.Commands;
+using CommandPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Classes
+{
+    internal class RemoteControl
+    {
+        private Command[] onCommands = new Command[7];
+        private Command[] offCommands = new Command[7];
+        private Stack<Command> undoStack = new Stack<Command>();
+
+        public RemoteControl()
+        {
+            Command noCommand = new NoCommand();
+            for (int i = 0; i < onCommands.Length; i++)
+            {
+                onCommands[i] = noCommand;
+                offCommands[i] = noCommand;
+            }
+        }
+
+        public void SetCommand(int slot, Command onCommand, Command offCommand)
+        {
+            onCommands[slot] = onCommand;
+            offCommands[slot] = offCommand;
+        }
+
+        public void OnButtonWasPushed(int slot)
+        {
+            onCommands[slot].Execute();
+            undoStack.Push(onCommands[slot]);
+        }
+
+        public void OffButtonWasPushed(int slot)
+        {
+            offCommands[slot].Execute();
+            undoStack.Push(offCommands[slot]);
+        }
+
+        public void UndoButtonWasPushed()
+        {
+            Command lastCommand = undoStack.Pop();
+            Console.WriteLine("undoing");
+            lastCommand.Undo();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("\n----- Remote Control ----- \n");
+            for (int i = 0; i < onCommands.Length; i++)
+            {
+                sb.Append("[slot " + i + "] " + onCommands[i] + " \t\t  " + offCommands[i] + "\n");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ CommandPattern/Program.cs
@@ -0,0 +1,58 @@
+﻿using CommandPattern.Classes;
+using CommandPattern.Classes.Commands;
+using CommandPattern.Interfaces;
+
+namespace CommandPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoteControl remoteControl = new RemoteControl();
+
+            /* Define and instantiate the following Vendor classes
+             * Kitchen Light : Light
+             * Livingroom Light : Light
+             * Livingroom ceiling fan : CeilingFan
+             * Garage door: Garagedoor
+             * Stereo : Stereo
+             */
+            Light kitchenLight = new Light("Kitchen");
+            Light livingRoomLight = new Light("Living Room");
+            CeilingFan livingRoomCeilingFan = new CeilingFan("Living Room");
+            GarageDoor garageDoor = new GarageDoor(new Light("Garage light"));
+            Stereo stereo = new Stereo();
+            // Define and instantiate an Off and On command for each Vendor class
+
+            /* Set the On and Off commands to the appropriate slot:
+             * 
+             * 1: Living Room light
+             * 2: Kitchen light
+             * 3: Livingroom ceiling fan
+             * 4: Garage door
+             * 5: Stereo
+             */
+            remoteControl.SetCommand(0, new LightOnCommand(livingRoomLight), new LightOffCommand(livingRoomLight));
+            remoteControl.SetCommand(1, new LightOnCommand(kitchenLight), new LightOffCommand(kitchenLight));
+            remoteControl.SetCommand(2, new CeilingFanHighCommand(livingRoomCeilingFan), new CeilingFanOffCommand(livingRoomCeilingFan));
+            remoteControl.SetCommand(3, new GarageDoorUpCommand(garageDoor), new GarageDoorDownCommand(garageDoor));
+            remoteControl.SetCommand(4, new StereoOnWithCdCommand(stereo), new StereoOffCommand(stereo));
+
+            Console.WriteLine(remoteControl);
+
+            // Test the pressing of Buttons here. Don't forget to test the Undo button
+
+            remoteControl.OnButtonWasPushed(0);
+            remoteControl.OffButtonWasPushed(0);
+            remoteControl.OffButtonWasPushed(3);
+            remoteControl.OnButtonWasPushed(3);
+            remoteControl.UndoButtonWasPushed();
+            remoteControl.UndoButtonWasPushed();
+            remoteControl.OnButtonWasPushed(2);
+            remoteControl.UndoButtonWasPushed();
+            remoteControl.OnButtonWasPushed(4);
+            remoteControl.UndoButtonWasPushed();
+            Console.ReadLine();
+        }
+    }
+}
